655e9ff39cdd2172cc07002215f61dec546379b1
48n02m41 <54042201+48n02m41@users.noreply.github.com>
2021-01-13 18:38:17 +0000

    Spring authentication dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,11 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation ("org.springframework.boot:spring-boot-starter-security")
+	implementation ("org.springframework.security:spring-security-oauth2-resource-server")
+	implementation ("org.springframework.security:spring-security-oauth2-jose")
+	implementation ("org.springframework.security:spring-security-config")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
