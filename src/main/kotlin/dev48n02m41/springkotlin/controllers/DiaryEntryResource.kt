9b850409c54267594ce5d9d988a7d8cc9df24f2f
48n02m41 <54042201+48n02m41@users.noreply.github.com>
2021-01-13 19:53:43 +0000

    Protect endpoints.

--- src/main/kotlin/dev48n02m41/springkotlin/controllers/DiaryEntryResource.kt
+++ src/main/kotlin/dev48n02m41/springkotlin/controllers/DiaryEntryResource.kt
@@ -2,22 +2,39 @@ package dev48n02m41.springkotlin.controllers
 
 import dev48n02m41.springkotlin.entities.DiaryEntryEntity
 import dev48n02m41.springkotlin.services.DiaryEntryService
+import net.minidev.json.JSONObject
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.*
+
 
 @RestController
-@RequestMapping("/v1/api/data")
+@RequestMapping(path = ["/v1/api"], produces = [MediaType.APPLICATION_JSON_VALUE])
+@CrossOrigin(origins = ["*"])
 class DiaryEntryResource (private val service: DiaryEntryService) {
 
-    @GetMapping
+    @GetMapping("/public/test", produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun testGET(): ResponseEntity<Any?>? {
+        return ResponseEntity.status(HttpStatus.OK).body(
+            Collections.singletonMap("status", "up")
+        )
+    }
+
+    @GetMapping("/private")
     fun getTasks(): List<DiaryEntryEntity> = service.getAll()
 
-    @PostMapping
+    @PostMapping("/private")
     fun addOne(@Validated @RequestBody entry: DiaryEntryEntity): ResponseEntity<DiaryEntryEntity> =
             service.addOne(entry)
 
-    @GetMapping("/{id}")
+    @GetMapping("/private/{id}")
     fun getOneById(@PathVariable(value = "id") idIn: Long): ResponseEntity<DiaryEntryEntity> =
             service.getById(idIn)
 
